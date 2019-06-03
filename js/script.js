05ec75152a9281a103aa4f29fc60cddebadb995a
juliaValerio <julia286@gmail.com>
2019-06-03 16:52:38 -0300

    projeto em progresso

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-var nav = document.querySelector('nav');
+const bgNav =  document.querySelector("nav")
 
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 0){
-            nav.style.background = "#ff00c863";
-        } 
-        else {
-            nav.style.background = "transparent";
-        }
-    });
+window.addEventListener("scroll", function(){
+  if (pageYOffset >= 50) {
+    bgNav.classList.remove("nav__bg")
+    bgNav.classList.add("nav__script")
+  }
+  if (pageYOffset < 49){
+    bgNav.classList.add("nav__bg")
+    bgNav.classList.remove("nav__script")
+  } 
+});
+
